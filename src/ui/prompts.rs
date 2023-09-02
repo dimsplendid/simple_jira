ef5ebb36a3cae66832150bbe868298511f102591
dimsplendid <dimsplendid@gmail.com>
2023-09-02 13:52:53 +0800

    Complete Stage 2-2: prompts

--- src/ui/prompts.rs
+++ src/ui/prompts.rs
@@ -21,21 +21,57 @@ impl Prompts {
 }
 
 fn create_epic_prompt() -> Epic {
-    todo!();
+    println!("----------------------------");
+    println!("Enter epic name: ");
+    let name = get_user_input();
+    println!("Enter epic description: ");
+    let description = get_user_input();
+    Epic::new(name, description)
 }
 
 fn create_story_prompt() -> Story {
-    todo!();
+    println!("----------------------------");
+    println!("Enter story name: ");
+    let name = get_user_input();
+    println!("Enter story description: ");
+    let description = get_user_input();
+    Story::new(name, description)
 }
 
 fn delete_epic_prompt() -> bool {
-    todo!();
+    println!("----------------------------");
+    println!(
+        "Are you sure you want to delete this epic? All stories in this epic will also be deleted [Y/n]:"
+    );
+    if let Some(input) = get_user_input().chars().next() {
+        input.to_ascii_lowercase() == 'y'
+    } else {
+        false
+    }
 }
 
 fn delete_story_prompt() -> bool {
-    todo!();
+    println!("----------------------------");
+    println!("Are you sure you want to delete this story? [Y/n]:");
+    if let Some(input) = get_user_input().chars().next() {
+        input.to_ascii_lowercase() == 'y'
+    } else {
+        false
+    }
 }
 
 fn update_status_prompt() -> Option<Status> {
-    todo!();
+    println!("----------------------------");
+    println!("New Status: (1 - OPEN, 2 - IN-PROGRESS, 3 - RESOLVED, 4 - CLOSED):");
+    if let Some(input) = get_user_input().chars().next() {
+        match input {
+            '1' => Some(Status::Open),
+            '2' => Some(Status::InProgress),
+            '3' => Some(Status::Resolved),
+            '4' => Some(Status::Closed),
+            _ => None
+        }
+    } else {
+        None
+    }
 }
